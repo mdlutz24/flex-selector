5589e61e28c11244453034b806a293d31334f770
Michael Lutz <mlutz531@gmail.com>
2018-09-01 17:50:27 -0400

    Add a header JS file

---
+++ web/js/footer.js
@@ -0,0 +1,18 @@
+/**
+ *  @file
+ **/
+
+(function ($, Drupal, window, document) {
+
+  'use strict';
+
+  Drupal.behaviors.myBehavior = {
+    attach: function (context, settings) {
+      $(document).ready(function () {
+
+
+      });
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
