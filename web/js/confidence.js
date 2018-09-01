e5902b60233b7a74e71204d774446f5c94b98d9f
Michael Lutz <mlutz531@gmail.com>
2018-09-01 16:29:13 -0400

    conf

--- web/js/confidence.js
+++ web/js/confidence.js
@@ -6,12 +6,62 @@ if (typeof franchise_id != 'undefined' &&
   (function ($, window, document, franchise_id, completedWeek, liveScoringWeek, thisProgram) {
     $(document).ready(function () {
       if (thisProgram == 'options_179') {
-        $form = $('form input[name=TYPE]').parent();
+        var $form = $('form input[name=TYPE]').parent();
         if (typeof $form == 'undefined') return;
-        $table = $form.find('table');
+        var $table = $form.find('table');
         if (typeof $table == 'undefined') return;
-        $rows = $table.find('tr');
-        $rows.shift()
+        var $rows = $table.find('tr');
+        var $hometeams = $('<div class="hometeams"></div>');
+        var $awayteams = $('<div class="awayteams"></div>');
+        var $picks = $('<div class="picks"></div>');
+        var $matches = [];
+
+        $rows.each(function(){
+          if (!$(this).hasClass('oddtablerow') && !$(this).hasClass('eventablerow')) return;
+          var $hometd = $(this).children('td:first-child');
+          var $homeradio = $hometd.children('input');
+          var $conf = $(this).children('td.inputfield select');
+          var $awaytd = $(this).children('td:nth-child(2)');
+          var $awayradio = $awaytd.children('input');
+
+          var $hometeam = $("<div class='hometeam'></div>");
+          $hometeam.innerText($hometd.children('label').innerText());
+          $hometeam.data('name', $homeradio.attr('name'));
+          $hometeam.data('value', $homeradio.attr('value'));
+
+          var $awayteam = $("<div class='awayteam'></div>");
+          $awayteam.innerText($awaytd.children('label').innerText());
+          $awayteam.data('name', $awayradio.attr('name'));
+          $awayteam.data('value', $awayradio.attr('value'));
+
+          if ($conf.val() != '-') {
+            $awayteam.data('conf', $conf.val());
+            $hometeam.data('conf', $conf.val());
+          }
+
+          if ($awayradio.is(':checked')) {
+            $awayteam.data('selected', true);
+            $awayteam.addClass('selected');
+            $matches.push($awayteam.clone());
+          }
+          if ($homeradio.is(':checked')) {
+            $hometeam.data('selected', true);
+            $hometeam.addClass('selected');
+            $matches.push($hometeam.clone());
+          }
+          $hometeams.append($hometeam);
+          $awayteams.append($awayteam);
+
+        })
+        $matches.sort(function(a,b){return a.data(conf)-b.data(conf)})
+        for ($match in $matches) {
+          $picks.append($match);
+        }
+        var $board = $("<div class='board'></div>");
+        $board.append($hometeams);
+        $board.append($awayteams);
+        $board.append($picks);
+        $form.after($board);
 
       }
 
